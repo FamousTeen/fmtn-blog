ecef12866872aea7c3aad56ea95d4867f962c9c8
FamousTeen <125239075+FamousTeen@users.noreply.github.com>
2025-03-16 19:31:00 +0700

    Navbar and Starting with Bento Grid

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/text-generate-effect'
+import MagicButton from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
 
 const Hero = () => {
   return (
-    <div className='pb-20 pt-36'>
+    <div className='pb-20 pt-8'>
       <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
@@ -19,9 +22,27 @@ const Hero = () => {
         />
       </div>
 
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+      <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      </div>
+
+      <div className='flex justify-center relative my-20 z-10'>
+        <div className="max-w-[89vw] md:max-w-2xl lg-max-w-[60vw] flex flex-col items-center justify-center">
+          <p className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
+            Welcome to my Portfolio
+          </p>
+
+          <TextGenerateEffect className='text-center text-[40px] md:text-5xl lg:text-6xl' words="Bring Seamless User Experience with Latest Features" />
+
+          <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
+            My name is  FmTn, a Full Stack Developer based in Indonesia
+          </p>
+
+          <a href="#about">
+            <MagicButton title="My Websites" icon={<FaLocationArrow />} iconPosition='right' />
+          </a>
+        </div>
       </div>
     </div>
   )
